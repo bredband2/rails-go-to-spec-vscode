5546bad5265804dfb746e1bf29086f8623f8cd7b
gabsul <gabrielsulka+github@gmail.com>
2025-02-20 10:19:07 +0100

    Go To Symbol

--- src/extension.ts
+++ src/extension.ts
@@ -4,10 +4,13 @@ import * as fs from "fs";
 import * as path from "path";
 import * as mkdirp from "mkdirp";
 
-function openFile(fileName: string) {
-	vscode.workspace
-		.openTextDocument(fileName)
-		.then(vscode.window.showTextDocument);
+async function openFile(fileName: string) {
+	return vscode.workspace.openTextDocument(fileName)
+}
+
+async function showDocument(document: vscode.TextDocument, position?: vscode.Position) {
+	const selection = position ? new vscode.Range(position, position) : undefined;
+	return vscode.window.showTextDocument(document, { selection });
 }
 
 function prompt(fileName: string, cb: any) {
@@ -44,7 +47,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand("rails-go-to-spec-2.railsGoToSpec", () => {
+	let disposable = vscode.commands.registerCommand("rails-go-to-spec-2.railsGoToSpec", async () => {
 		// Display a message box to the user
 		var editor = vscode.window.activeTextEditor;
 		if (!editor) {
@@ -56,12 +59,105 @@ export function activate(context: vscode.ExtensionContext) {
 		// Get a list of related files
 		// if any of those exists, open it
 		// Otherwise prompt to create the first one
+		const fromSpec = resolver.isSpec(fileName);
 		let related: Array<string> = resolver.getRelated(fileName);
 
 		for (let relatedFile of related) {
 			let fileExists: boolean = fs.existsSync(relatedFile);
 			if (fileExists) {
-				openFile(relatedFile);
+				const relatedDocument = await openFile(relatedFile);
+				let position: vscode.Position | undefined = undefined;
+
+				if (fromSpec) {
+					const srcUri = vscode.Uri.parse(relatedFile)
+
+					const currentLine = editor.selection.start.line;
+					const line = editor.document.getText()
+						.split("\n")
+						.slice(0, currentLine + 1)
+						.map((line, lineNo) => {
+							const m = line.match(/\s*describe\s+"([#.])(\w+[\?\!]?)"\s+do\s*/)
+							if (m) {
+								const name = m[1] == "." ? "self." + m[2] : m[2];
+								return { name, lineNo }
+							}
+							return undefined
+						})
+						.reverse()
+						.find(b => !!b);
+
+					if (line) {
+						const symbolName = line.name
+						const symbols: (vscode.SymbolInformation | vscode.DocumentSymbol)[] = await vscode.commands.executeCommand('vscode.executeDocumentSymbolProvider', srcUri);
+
+						function findSymbolByName(nodes: (vscode.SymbolInformation | vscode.DocumentSymbol)[], name: string): vscode.SymbolInformation | vscode.DocumentSymbol | undefined {
+							for (let node of nodes) {
+								if (node.name === name) {
+									return node;
+								}
+
+								if ((node as vscode.DocumentSymbol).children) {
+									const symbol = findSymbolByName((node as vscode.DocumentSymbol).children, name);
+									if (symbol) {
+										return symbol;
+									}
+								}
+							}
+							return undefined;
+						};
+
+						const symbol = findSymbolByName(symbols, symbolName);
+						if (symbol instanceof vscode.SymbolInformation) {
+							position = symbol.location.range.start;
+						} else if (symbol) {
+							position = symbol.range.start;
+						}
+					}
+				} else {
+					function getRange(symbol: vscode.SymbolInformation | vscode.DocumentSymbol) {
+						if (symbol instanceof vscode.SymbolInformation) {
+							return symbol.location.range;
+						} else if (symbol) {
+							return symbol.range;
+						}
+						const p = new vscode.Position(-1, -1);
+						return new vscode.Range(p, p)
+					}
+
+					function getChildren(symbol: vscode.SymbolInformation | vscode.DocumentSymbol) {
+						return (symbol as vscode.DocumentSymbol).children || [];
+					}
+
+					const srcUri = editor.document.uri
+					const symbols: (vscode.SymbolInformation | vscode.DocumentSymbol)[] = await vscode.commands.executeCommand('vscode.executeDocumentSymbolProvider', srcUri);
+
+					function findSymbolByPosition(nodes: (vscode.SymbolInformation | vscode.DocumentSymbol)[], position: vscode.Position): vscode.SymbolInformation | vscode.DocumentSymbol | undefined {
+						for (let node of nodes) {
+							if (getRange(node).contains(position)) {
+								if (getChildren(node).length <= 0) {
+									return node;
+								} else {
+									return findSymbolByPosition(getChildren(node), position);
+								}
+							}
+						}
+						return undefined;
+					};
+
+					const symbol = findSymbolByPosition(symbols, editor.selection.start);
+					if (symbol) {
+						const specName = symbol.name.startsWith("self.") ? ("." + symbol.name.substring(5)) : ("#" + symbol.name);
+						const index = relatedDocument.getText()
+							.split("\n")
+							.findIndex(line => line.includes("\"" + specName + "\""));
+
+						if (index >= 0) {
+							position = new vscode.Position(index, 0);
+						}
+					}
+				}
+
+				showDocument(relatedDocument, position);
 				break;
 			}
 		};
